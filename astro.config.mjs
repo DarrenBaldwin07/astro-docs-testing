27e65f62fdea714ac33c8c4b872d0df2b40935e8
Darren B <68653294+Devd0@users.noreply.github.com>
2023-11-06 21:24:17 -0500

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,35 @@ import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
     starlight({
       title: "My Docs",
+      customCss: [
+        // Path to your Tailwind base styles:
+        "./src/tailwind.css",
+      ],
       social: {
         github: "https://github.com/withastro/starlight",
       },
-
       sidebar: [
         {
           label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", link: "/guides/example/" },
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
           ],
         },
         {
           label: "Reference",
-          autogenerate: { directory: "reference" },
+          autogenerate: {
+            directory: "reference",
+          },
         },
       ],
     }),
